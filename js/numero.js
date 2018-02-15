9a4301f06121307ed415fe489577627cc27bf2bf
karkuu <36222082+karkuu@users.noreply.github.com>
2018-02-15 13:08:12 +0200

    Numero

--- js/numero.js
+++ js/numero.js
@@ -1,27 +1,55 @@
 "use strict";
 class arvaaNumero
 {
-	constructor(suurinnumero,kohde)
+	constructor(suurinnumero,kohde, omaNimi)
 	{
 		this.suurinnumero = suurinnumero; // Suurin satunnainen numero
 		this.kohde = kohde;
-		this.arvattavanumero = -1;
+		this.omaNimi = omaNimi;
+		this.arvattavanumero = 500;
+		this.arvauksia = 0;
 		
 		this.lisaaElementitSivulle();
+		this.arvoNumero();
     }
 	
 	arvoNumero()
 	{
 		this.arvattavanumero = Math.floor(Math.random()*this.suurinnumero);
+		document.getElementById("piilotusboxi").innerHTML = this.arvattavanumero;
 	}
-	arvaaNumeroa()
+	arvausbuttonpress()
 	{
+		var ilmoitustxt = "";
+		this.arvauksia++;		
 		
+		if (document.getElementById("arvaustxt").value == this.arvattavanumero)
+		{
+			document.getElementById("teksti").innerHTML = "Oikein<br>Arvauksia: "+this.arvauksia;
+			document.getElementById('piilotusboxi').style.backgroundColor = 'white';
+		}
+		else
+		{
+			if (this.arvattavanumero/document.getElementById("arvaustxt").value > 1.25 || this.arvattavanumero/document.getElementById("arvaustxt").value < 0.83333333)
+			{
+				ilmoitustxt += "paljon ";
+			}
+			if (this.arvattavanumero/document.getElementById("arvaustxt").value < 1)
+			{
+				ilmoitustxt += "pienempi";
+			}
+			else if (this.arvattavanumero/document.getElementById("arvaustxt").value > 1)
+			{
+				ilmoitustxt += "suurempi";
+			}
+			document.getElementById("teksti").innerHTML = "Väärin! Luku on "+ilmoitustxt+"<br>Arvauksia: "+this.arvauksia;
+		}
 	}
 	lisaaElementitSivulle()
 	{
-		var sisalto = "<table border=1><tr><td id=\"piilotusboxi\"></td></tr><tr><td id=\"teksti\"></td></tr><tr><td id=\"arvausboxi\"><input type=\"textfield\" id=\"arvaustxt\" maxlength=\"3\"></td></tr><tr><td id=\"nappiboxi\"><input type=\"button\" value=\"Arvaa!\" id=\"arvausbutton\" onclick=\"arvausbuttonpress();\"></td></tr><table>";
-		
+		var sisalto = "<table border=1><tr><td id=\"piilotusboxi\"></td></tr><tr><td id=\"teksti\"></td></tr><tr><td id=\"arvausboxi\"><input type=\"textfield\" id=\"arvaustxt\" maxlength=\"3\"></td></tr><tr><td id=\"nappiboxi\"><input type=\"button\" value=\"Arvaa!\" id=\"arvausbutton\" onclick=\""+this.omaNimi+".arvausbuttonpress();\"></td></tr><table>";
+
 		document.getElementById(this.kohde).innerHTML = sisalto;
 	}
 }
+
