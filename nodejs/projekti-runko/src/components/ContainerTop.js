6e7a7669e8993e717abf3f7e53213ab998b2a70b
karkuu <36222082+karkuu@users.noreply.github.com>
2018-03-07 11:46:50 +0200

    add

---
+++ nodejs/projekti-runko/src/components/ContainerTop.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import TopMenu from './TopMenu';
+
+
+export default class ContainerTop extends React.Component {
+  render() {
+    return (
+      <div>
+	  containertop
+     <TopMenu/>
+      </div>
+    );
+  }
+}
---
+++ nodejs/projekti-runko/src/components/TopMenu.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class TopMenu extends React.Component {
+  render() {
+    return (
+      <div>
+	  topmenu
+      </div>
+    );
+  }
+}
