17eb46dcaece0e67baa5f720f21fc09389aafb6d
karkuu <36222082+karkuu@users.noreply.github.com>
2018-04-04 11:05:56 +0300

    events

--- angular/angular-app/src/app/new-cmp/new-cmp.component.ts
+++ angular/angular-app/src/app/new-cmp/new-cmp.component.ts
@@ -11,9 +11,31 @@ export class NewCmpComponent implements OnInit {
   testvar = "Test variable.";
 
    // declared array of months.
-   months = ["January", "Feburary", "March", "April", "May", 
+  months = ["January", "Feburary", "March", "April", "May", 
             "June", "July", "August", "September",
             "October", "November", "December"];
+  selectedMonth = "";
+  isavailable = false;   //variable is set to true
+
+  
+  myClickFunction(event) {
+    if (this.selectedMonth =="")
+    {
+      alert("Month hasn't been selected!");
+    }
+    else {
+      alert("Selected month is "+this.selectedMonth);
+    }
+    
+    console.log(event);
+  }
+  changemonths(event) {
+    this.isavailable = true;
+    this.selectedMonth = event.target.value;
+    console.log(event);
+  }  
+
+
 
   ngOnInit() {
   }
