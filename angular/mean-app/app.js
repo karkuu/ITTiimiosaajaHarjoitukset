04d339dd5537e036a3ac7f256a24a149ba24df49
karkuu <36222082+karkuu@users.noreply.github.com>
2018-04-05 11:45:16 +0300

    edits

--- angular/mean-app/app.js
+++ angular/mean-app/app.js
@@ -9,7 +9,7 @@ var app = express();
 
 var mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
-mongoose.connect('mongodb://localhost/mean-angular5', { useMongoClient: true, promiseLibrary: require('bluebird') })
+mongoose.connect('mongodb://localhost/mean-angular5', { })
   .then(() =>  console.log('connection succesful'))
   .catch((err) => console.error(err));
 
