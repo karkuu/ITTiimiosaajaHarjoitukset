966b02ee67ec2a2dc76b21d239479a799807436f
karkuu <36222082+karkuu@users.noreply.github.com>
2018-05-14 12:17:28 +0300

    conditional.vue

---
+++ nodejs/harjoitus/vue/conditionalevent/src/components/Conditional.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <div v-if="isLogged">
+            <label>Logout</label>
+            <button v-on:click="logout()">Logout</button>
+        </div>
+        <div v-else>
+            <label>Login</label>
+            <button v-on:click="login()">Logit</button>
+        </div>
+        <p v-show="isLogged">
+            Will not show if not logged in. This component will still be rendered.
+        </p>
+    </div>
+</template>
+<script>
+export default {
+    name:"Conditional",
+    data() {
+        return {
+            isLogged:false
+        }
+    },
+    methods: {
+        login: function(event) {
+            this.isLogged = true;
+        },
+        logout: function(event) {
+            this.isLogged = false;
+        }
+    }
+}
+</script>
