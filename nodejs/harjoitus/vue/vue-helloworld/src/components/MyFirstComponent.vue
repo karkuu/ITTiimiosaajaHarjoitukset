1928e91c44aceba454712a154ccde7d19db4c6cb
karkuu <36222082+karkuu@users.noreply.github.com>
2018-05-14 10:48:36 +0300

    vue

---
+++ nodejs/harjoitus/vue/vue-helloworld/src/components/MyFirstComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }} {{myname}}</h1>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyFirstComponent',
+  props: ['myname'],
+  data () {
+    return {
+      msg: 'Hello'
+    }
+  }
+}
+</script>
