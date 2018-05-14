eb53b883364ec96423a039090af890f9e230f226
karkuu <36222082+karkuu@users.noreply.github.com>
2018-05-14 13:28:33 +0300

    customevent

---
+++ nodejs/harjoitus/vue/customevent/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <h1> Child component says: {{msg}}</h1>
+    <CustomEvent @my-custom-event="handleEvent(...arguments)"/>
+  </div>
+</template>
+
+<script>
+import CustomEvent from './components/CustomEvent'
+
+export default {
+  name: 'App',
+  components: {
+    CustomEvent
+  },
+  data () {
+    return {
+      msg: ""
+    }
+  },
+  methods: {
+    handleEvent: function(data) {
+      this.msg = data;
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ nodejs/harjoitus/vue/customevent/src/components/CustomEvent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+      <p>Enter message and press button to send</p>
+      <input v-model="newmessage" type="text"/>
+      <button v-on:click="sendMessage()">Send</button>
+  </div>
+</template>
+<script>
+export default {
+  name:"CustomEvent",
+  data () {
+      return {
+          newmessage:""
+      }
+  },
+  methods: {
+      sendMessage: function() {
+          this.$emit("my-custom-event",this.newmessage);
+          this.newmessage = "";
+      }
+  }
+}
+</script>
+
