8c0d08b9dd68433d5b729ea350fd652c539c7485
karkuu <36222082+karkuu@users.noreply.github.com>
2018-05-14 12:54:09 +0300

    listrendering

---
+++ nodejs/harjoitus/vue/listrendering/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <ListRendering/>
+  </div>
+</template>
+
+<script>
+import ListRendering from './components/ListRendering'
+
+export default {
+  name: 'App',
+  components: {
+    ListRendering
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ nodejs/harjoitus/vue/listrendering/src/components/ListRendering.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <label>Enter new message for the list:</label>
+        <input v-model="newmessage" v-on:keyup.enter="addNew()"/>
+        
+        <ul v-if="items.length">
+            <li v-for="(item,index) in items" :key="item.id">
+                {{index+1}}.{{item.message}}
+                <button v-on:click="remove(index)">Remove</button>
+            </li>
+        </ul>
+        <p v-else>
+            Nothing on the list.
+        </p>
+    </div>
+</template>
+<script>
+export default {
+    name:"ListRendering",
+    data() {
+        return {
+            items:[
+                {message:"Hello world!",id:1},
+                {message:"Terve maailma!",id:2},
+                {message:"Moi.",id:3},
+            ],
+            currentId:4,
+            message:""         
+        }
+    },
+    methods: {
+        addNew: function() {
+           this.items.push({message:this.newmessage,id:this.currentId});
+           this.currentId++;
+           this.newmessage="";
+        },
+        remove: function(index) {
+            this.items.splice(index,1);
+        }
+    }
+}
+</script>
